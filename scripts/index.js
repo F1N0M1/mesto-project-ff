442bf68b03bf10f15d43ada08a5d7ce566555cd8
unknown <varod9997@gmail.com>
2023-12-05 23:49:18 +0700

    add delete, not work

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,16 @@ const test = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-addCardToPage(test);
+
+addCardToPage(initialCards);
+// addCardToPage(test);
 
 const btnOpenPopupAddCard = document.querySelector(".profile__add-button");
 const popupAdd = document.querySelector(".popup_type_new-card");
 const popupClose = document.querySelector(".popup__close");
 
 const deleteCard = document.querySelector(".card__delete-button");
+
 
 btnOpenPopupAddCard.addEventListener("click", () => {
   popupAdd.classList.add("popup_is-opened");
@@ -29,10 +32,18 @@ function addCardToPage(cards) {
     cardElement.querySelector(".card__image").src = item.link;
     cardElement.querySelector(".card__title").textContent = item.name;
 
-    cardList.append(cardElement);
+    cardList.prepend(cardElement);
   });
 }
 // @todo: Функция удаления карточки
 
+deleteCard.addEventListener("click", (evt) => {
+  const card = evt.target.closest("li");
+  console.log(evt.currentTarget);
+  console.log(evt.target);
+  card.remove();
+});
+
 // @todo: Вывести карточки на страницу
-addCardToPage(initialCards);
+
+
