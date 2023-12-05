833f9bf8a0a579081522839c97a4936c13497040
Родичев Валерий Павлович <rodichev_vp@sb.sbpriv>
2023-12-05 16:45:09 +0700

    js add cards to page

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
+function loadStart() {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardList = document.querySelector('.places__list');
+
+    initialCards.forEach(function(item) {
+        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+        cardElement.querySelector('.card__image').src = item.link;
+        cardElement.querySelector('.card__title').textContent = item.name;
+
+        cardList.append(cardElement);
+    });
+}
+
+loadStart();
+// cardList.append(cardElement);
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
