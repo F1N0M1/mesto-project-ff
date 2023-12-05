d372d3eddb4adbc61c1100da84f2a52c76abb675
Родичев Валерий Павлович <rodichev_vp@sb.sbpriv>
2023-12-05 17:59:39 +0700

    add js open popup add card

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,38 @@
-function loadStart() {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardList = document.querySelector('.places__list');
+const test = [
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+addCardToPage(test);
 
-    initialCards.forEach(function(item) {
-        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+const btnOpenPopupAddCard = document.querySelector(".profile__add-button");
+const popupAdd = document.querySelector(".popup_type_new-card");
+const popupClose = document.querySelector(".popup__close");
 
-        cardElement.querySelector('.card__image').src = item.link;
-        cardElement.querySelector('.card__title').textContent = item.name;
+const deleteCard = document.querySelector(".card__delete-button");
 
-        cardList.append(cardElement);
-    });
-}
-
-loadStart();
-// cardList.append(cardElement);
-
+btnOpenPopupAddCard.addEventListener("click", () => {
+  popupAdd.classList.add("popup_is-opened");
+});
+//popup_is-opened
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function addCardToPage(cards) {
+  cards.forEach(function (item) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardList = document.querySelector(".places__list");
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
+    cardElement.querySelector(".card__image").src = item.link;
+    cardElement.querySelector(".card__title").textContent = item.name;
+
+    cardList.append(cardElement);
+  });
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+addCardToPage(initialCards);
