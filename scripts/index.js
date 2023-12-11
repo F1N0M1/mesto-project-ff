65240d446947ed1414e24255fb7a973e6f9f2e92
unknown <varod9997@gmail.com>
2023-12-11 19:54:43 +0700

    add readme; 2 try

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,24 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 const cardList = document.querySelector(".places__list");
-const popupClose = document.querySelector(".popup__close");
 
 // @todo: Функция создания карточки
-function addCardToPage(name, link, del) {
+function addCardToPage(card, onDelete) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  deleteButton = cardElement.querySelector(".card__delete-button");
-  cardElement.querySelector(".card__image").src = link;
-  cardElement.querySelector(".card__image").alt = name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
   cardElement.querySelector(".card__title").textContent = name;
-  deleteButton.addEventListener("click", (del));
+  deleteButton.addEventListener("click", onDelete);
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function removeCard(event) {
+function handleRemoveCard(event) {
   const card = event.target.closest(".places__item");
   card.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (item) {
-  cardList.prepend(addCardToPage(item.name, item.link, removeCard));
+  cardList.prepend(addCardToPage(item, handleRemoveCard));
 });
